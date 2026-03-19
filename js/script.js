ad18a253937c90d0fec0b4a85843232bf6b6f7cc
Maria-gonzaga <maria.9553712@aluno.mg.gov.br>
2026-03-19 14:19:11 +0000

    uddate js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 let likeCount = 0;
+let curtido = false; //flag booleana
 
 function curtir() {
+  if(curtido== true){
   likeCount++;
   document.getElementById("likeCount").innerText = likeCount;
+  }else{
+
+  likeCount--;
+  curtido=false;
+  document.getElementById("likeCount").innerText = likeCount;
+
+    
+  }
+
+
+
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
